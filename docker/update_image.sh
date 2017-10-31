f3d80a63a964c3ed759be823ce1947291d3ed6d0
Brendan Long <self@brendanlong.com>
2017-10-31 19:38:40 -0400

    Move Feedbin to its own Docker image

--- docker/update_image.sh
+++ docker/update_image.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
+FEEDBIN_NAME=docker.io/feedreader/feedbin
 NAME=docker.io/feedreader/fedora-feedreader-devel
+
+sudo docker build -f Dockerfile.Feedbin . -t "$FEEDBIN_NAME"
+sudo docker push "$FEEDBIN_NAME"
 
 sudo docker build . -t "$NAME"
 sudo docker push "$NAME"
